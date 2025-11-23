1cd4b308f03bba268750ae7886f64eab04cd10e1
Muhammad Teguh Satria <teguh@akarprima.com>
2025-11-23 22:58:52 +0700

    update tugas 3

---
+++ tugas_3/Gaji.java
@@ -0,0 +1,47 @@
+package tugas_3;
+
+import java.util.Scanner;
+
+public class Gaji {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int gaji[] = {5000000, 6500000, 9500000};
+        double lembur[] = {30, 32, 34, 36, 38};
+
+        System.out.print("Masukan golongan (A/B/C) : ");
+        String golongan = input.nextLine().toUpperCase();
+
+        int gajiTmp = 0;
+        switch (golongan) {
+            case "A":
+                gajiTmp = gaji[0];
+                break;
+            case "B":
+                gajiTmp = gaji[1];
+                break;
+            case "C":
+                gajiTmp = gaji[2];
+                break;
+            default:
+                System.out.println("Golongan tidak diketahui");
+                System.exit(0);
+        }
+
+        System.out.print("Masukan jam lembur : ");
+        int jamLembur = input.nextInt();
+        double persen = 0;
+
+        if (jamLembur >= 1) {
+            if (jamLembur >= 5) {
+                persen = lembur[4] / 100;
+            } else {
+                persen = lembur[jamLembur - 1] / 100;
+            }
+        }
+
+
+        int takeHomePay = (int) (gajiTmp * persen) + gajiTmp;
+        System.out.println("Take Home Pay " + takeHomePay);
+    }
+}
---
+++ tugas_3/readme.md
@@ -0,0 +1,8 @@
+Preview
+
+1. Buka terminal lalu jalankan command java tugas_3.Gaji
+2. Masukan Golongan gaji
+3. Masukan Jam lembur
+
+
+![alt text](../assets/1.png)
